da45ceae92f2bd6e1009434fdf5013b3c4201ece
JoaoMaciel5G <juauvitu765@gmail.com>
2024-02-17 15:25:28 -0300

    add limit archive

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/route"
+import bodyParser from "body-parser"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use("/", router)
 
 export default app
